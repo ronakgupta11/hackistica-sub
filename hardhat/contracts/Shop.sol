e2515aa8b42608a541fa180f71773379e071d9d9
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-01-16 17:07:58 +0530

    graph added

--- hardhat/contracts/Shop.sol
+++ hardhat/contracts/Shop.sol
@@ -3,22 +3,25 @@ pragma solidity ^0.8.17;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Shop at 0xcF8C5C5Cd9762798e4e33fF770c91c6639fee287
+// https://mumbai.polygonscan.com/address/0xcF8C5C5Cd9762798e4e33fF770c91c6639fee287#code
 
 contract Shop is Ownable {
-    ;
+    uint id = 0;
+        uint shopEarning = 0;
     constructor(){
         address owner = msg.sender;
-        uint id = 0;
-        uint shopEarning = 0;
+        
     }
-    event productCreated(uint productId,string name, string category,uint price,string token,address createdBy);
+    event productCreated(uint productId,string imageUrl,string name, string category,uint price,string token,uint quantity,address createdBy);
 
     event productBuy(uint productId, uint quantityBought, address buyer);
 
     event productAddToCart(uint productId);
 
-    event removeFromCart(uint productId);
+    event productRemoveFromCart(uint productId);
 
     struct Product{
         uint productId;
@@ -41,14 +44,14 @@ contract Shop is Ownable {
     mapping (address => uint) public sellerBalance;
     //create a object for product containing productId,price,name,details,img
 
-    function platformFee(uint _price){
-        return (price * 999)/1000;
+    function platformFee(uint _price) pure internal returns(uint){
+        return (_price * 999)/1000;
 
 
     }
 
     function buy(uint _productId, uint _quantity) public payable{
-        Product selectedProduct = products[_productId];
+        Product memory selectedProduct = products[_productId];
         uint price = _quantity*selectedProduct.price;
         require(msg.value>price,"sent value is less then price");
         require(products[_productId].quantity>0,"sold out");
@@ -68,13 +71,13 @@ contract Shop is Ownable {
         
     }
 
-    function listProduct(string _image, string _name, string _category,uint _price,uint _quantity,string _token) public{
+    function listProduct(string memory _image, string memory _name, string memory _category,uint _price,uint _quantity,string memory _token) public{
         uint productId = id;
-        products.push(Product(productId,_image,_name,_category,_price,_quantity,_token,msg.sender));
+        products.push(Product(productId,_image,_name,_category,_price,_token,_quantity,msg.sender));
         id++;
 
         //emit event productCreated
-        emit productCreated(productId,_image,_name,_category,_price,_quantity,_token,msg.sender);
+        emit productCreated(productId,_image,_name,_category,_price,_token,_quantity,msg.sender);
     }
     
     function addToCart(uint _productId) public{
@@ -83,12 +86,12 @@ contract Shop is Ownable {
     }
 
     function removeFromCart(uint _productId) public{
-        emit removeFromCart(_productId);
+        emit productRemoveFromCart(_productId);
     }
 
 
     function withdrawSeller() public{
-        (bool sent,) = msg.sender.call(){value: sellerBalance[msg.sender] }("");
+        (bool sent,) = msg.sender.call{value: sellerBalance[msg.sender] }("");
         require(sent,"failed to sent ether to seller");
         sellerBalance[msg.sender]= 0;
 
@@ -97,7 +100,7 @@ contract Shop is Ownable {
 
 
     function withdrawOwner() public onlyOwner{
-        (bool sent,) = msg.sender.call(){value: shopEarning }("");
+        (bool sent,) = msg.sender.call{value: shopEarning }("");
         require(sent,"failed to sent ether");
         shopEarning = 0;
 
